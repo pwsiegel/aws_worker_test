6194112131f91743db1b843b3a9859d2ec22bc9f
Paul Siegel <pwsiegel@gmail.com>
2017-07-17 17:26:59 -0400

    renamed bucket to worker

--- application.py
+++ application.py
@@ -41,7 +41,7 @@ def get_from_s3():
 def worker():
 	'''
 	Takes a JSON of the form {"file": "myfile", "message": "mymessage"} and writes "mymessage" to a file named
-	"myfile" in the S3 bucket "bwworker".
+	"myfile" in the S3 bucket "worker".
 	The JSON is sent to the worker by submitting a POST request to the worker's SQS queue. (Though note that if
 	this application is deployed to both the web server and the worker then it is possible to POST directly
 	to the web server.)
@@ -55,7 +55,7 @@ def worker():
 @application.route("/tasks", methods = ["POST"])
 def tasks():
 	'''
-	Opens the file "tasks" in the S3 bucket "bwworker", reads whitespace delimited file names, and for each
+	Opens the file "tasks" in the S3 bucket "worker", reads whitespace delimited file names, and for each
 	file name reads an integer from the file, adds 1, and writes the result.
 	This URL is called automatically at time(s) specified in the file "cron.yaml" in the same directory as 
 	this application.  (As above, note that if this application is deployed to the web server as well as the
@@ -73,13 +73,13 @@ def tasks():
 
 def read_s3(file_name):
 	try:
-		return boto3.resource("s3").Object(bucket_name = "bwworker", key = file_name).get()["Body"].read().decode("utf-8")
+		return boto3.resource("s3").Object(bucket_name = "worker", key = file_name).get()["Body"].read().decode("utf-8")
 	except botocore.exceptions.ClientError as e:
 		return str(e)
 
 def write_s3(file_name, message):
 	try:
-		boto3.resource("s3").Bucket("bwworker").put_object(Key = file_name, Body = str(message))
+		boto3.resource("s3").Bucket("worker").put_object(Key = file_name, Body = str(message))
 		return "Message sent"
 	except botocore.exceptions.ClientError as e:
 		return str(e)
